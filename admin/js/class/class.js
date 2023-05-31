2e686bee38c87402faa9bf1e5679708e6b7efb40
wldms97 <133258648+wldms97@users.noreply.github.com>
2023-05-31 13:48:50 +0900

    Add files via upload

---
+++ admin/js/class/class.js
@@ -0,0 +1,11 @@
+// class_add.php 파일첨부 시 이름이 파일명으로 바뀔 수 있게 하는 스크립트
+let fileTarget = document.querySelector('.cls_thumb #cls_thumb');
+  
+  fileTarget.addEventListener('change', function() {
+    let filename = "";
+    if (window.FileReader) {
+      filename = this.files[0].name;
+    }
+    // 추출한 파일명 삽입
+    this.nextElementSibling.value = filename;
+  });
